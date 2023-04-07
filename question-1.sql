6aed637c857d136699d200282e64c3d3a2e8e396
nikkisuneel <53950214+nikkisuneel@users.noreply.github.com>
2023-04-06 17:06:01 -0700

    Update question-1.sql

--- question-1.sql
+++ question-1.sql
@@ -1,1 +1,13 @@
-
+ SELECT first_name
+   ...> FROM persons
+   ...> WHERE city = 'Omaha';
+ Answer
++------------+
+| first_name |
++------------+
+| Duky       |
+| Amity      |
+| Elena      |
+| Lynn       |
+| Valentijn  |
++------------+
