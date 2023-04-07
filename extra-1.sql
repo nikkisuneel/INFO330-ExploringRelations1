e83c2631b7392e654e13c8cec18e327791b6011c
nikkisuneel <53950214+nikkisuneel@users.noreply.github.com>
2023-04-06 18:32:38 -0700

    Update extra-1.sql

--- extra-1.sql
+++ extra-1.sql
@@ -1,1 +1,22 @@
+sqlite> SELECT age
+   ...> FROM persons
+   ...> WHERE (city = 'West Palm Beach' AND state = 'Florida' AND gender = 'Female' AND title = 'Dr')
+   ...> ;
++-----+
+| age |
++-----+
+| 49  |
++-----+
+sqlite> 
 
+sqlite> SELECT age
+   ...> FROM persons
+   ...> WHERE (city = 'Phoenix' AND state = 'Arizona' AND gender = 'Male' AND title = 'Rev');
++-----+
+| age |
++-----+
+| 101 |
++-----+
+sqlite> 
+
+Answer: NO they are not the same age
