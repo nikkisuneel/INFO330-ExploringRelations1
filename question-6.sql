30d30960e1148a85058e0dfc9e1fad83f6411306
nikkisuneel <53950214+nikkisuneel@users.noreply.github.com>
2023-04-06 17:38:09 -0700

    Update question-6.sql

--- question-6.sql
+++ question-6.sql
@@ -1,1 +1,12 @@
-
+sqlite> SELECT DISTINCT substr(email, instr(email, '@') + 1) AS email_domain
+   ...> FROM persons
+   ...> WHERE city = 'Seattle';
++--------------+
+| email_domain |
++--------------+
+| is.gd        |
+| msn.com      |
+| amazon.com   |
+| dyndns.org   |
++--------------+
+sqlite> 
