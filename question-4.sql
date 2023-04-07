8a9f88f0106d1a6300abfdef5bc4cabc5b649d62
nikkisuneel <53950214+nikkisuneel@users.noreply.github.com>
2023-04-06 17:29:22 -0700

    Update question-4.sql

--- question-4.sql
+++ question-4.sql
@@ -1,1 +1,24 @@
-
+sqlite> SELECT first_name, last_name
+   ...> FROM persons
+   ...> WHERE age = (SELECT MIN(age) FROM persons);
++------------+-----------+
+| first_name | last_name |
++------------+-----------+
+| Corbet     | Niess     |
+| Noella     | O'Hickee  |
+| Vicky      | Matitiaho |
+| Jill       | Domenico  |
+| Irving     | Feveryear |
+| Cinderella | Guilder   |
+| Meir       | Brake     |
+| Mariquilla | Gwyllt    |
+| Krystle    | Varah     |
+| Philippa   | Canfield  |
+| Irvin      | Ilyenko   |
+| Kizzie     | Hawkin    |
+| Eveline    | Skeermor  |
+| Ania       | Knottley  |
+| Jennica    | Totman    |
+| Tatiana    | Leaf      |
++------------+-----------+
+sqlite> 
