5c74dd92efb8a5a9d875338a9633547e516e4228
nikkisuneel <53950214+nikkisuneel@users.noreply.github.com>
2023-04-06 17:24:32 -0700

    Update question-3.sql

--- question-3.sql
+++ question-3.sql
@@ -1,1 +1,94 @@
-
+sqlite> SELECT first_name, last_name
+   ...> FROM persons
+   ...> WHERE gender = 'Female' AND title LIKE '%Rev%'
+   ...> ORDER BY last_name;
++-----------------+----------------+
+|   first_name    |   last_name    |
++-----------------+----------------+
+| Tawsha          | Alberts        |
+| Karoline        | Allmann        |
+| Linette         | Amis           |
+| Karalynn        | Boniface       |
+| Kristien        | Broschek       |
+| Philippa        | Canfield       |
+| Avis            | Castenda       |
+| Kaycee          | Causey         |
+| Keelia          | Cawcutt        |
+| Mikaela         | Chown          |
+| Larina          | Coate          |
+| Delilah         | Cockshot       |
+| Karin           | Cowling        |
+| Kendre          | Dewan          |
+| Hadria          | Dripps         |
+| Caryl           | Dunbavin       |
+| Hedvig          | Edelmann       |
+| Gray            | Flores         |
+| Lurleen         | Fosberry       |
+| Trista          | Goadbie        |
+| Helen-elizabeth | Goff           |
+| Hazel           | Gotmann        |
+| Jo-ann          | Grieve         |
+| Daria           | Hamsher        |
+| Trudie          | Haucke         |
+| Kizzie          | Hawkin         |
+| Marlena         | Hearnshaw      |
+| Caroljean       | Hector         |
+| Elsey           | Hollyman       |
+| Ferdinande      | Huffa          |
+| Elga            | Iacovielli     |
+| Charmain        | Ilbert         |
+| Linet           | Jacquemy       |
+| Milicent        | Joselevitz     |
+| Lyndsay         | Kieran         |
+| Brita           | Kiff           |
+| Corri           | Klement        |
+| Carmencita      | Kohen          |
+| Janina          | Lansdowne      |
+| Chiarra         | Larkings       |
+| Tracey          | Leydon         |
+| Rozina          | Lindblad       |
+| Danielle        | Lucken         |
+| Midge           | MacFaell       |
+| Brena           | MacNockater    |
+| Demetris        | Makepeace      |
+| Katey           | Manilove       |
+| Geraldine       | Martins        |
+| Sunny           | McAllister     |
+| Felipa          | McCraine       |
+| Sophi           | McTrustrie     |
+| Robena          | Micallef       |
+| Karla           | Muddimer       |
+| Lois            | Muggleston     |
+| Kim             | Murton         |
+| Anne-marie      | O'Flannery     |
+| Mercy           | Olesen         |
+| Suzanne         | Peers          |
+| Claudette       | Poor           |
+| Florentia       | Priditt        |
+| Gwendolyn       | Ramalhete      |
+| Hedvige         | Reubbens       |
+| Jaimie          | Revance        |
+| Tamarah         | Rockhill       |
+| Clemmie         | Safell         |
+| Deeann          | Schriren       |
+| Jennica         | Slader         |
+| Carmela         | Sonley         |
+| Martynne        | Staynes        |
+| Georgianne      | Thrasher       |
+| Persis          | Tiddeman       |
+| Babbette        | Timberlake     |
+| Jennica         | Totman         |
+| Talyah          | Trubshawe      |
+| Aryn            | Truckett       |
+| Lorelei         | Twinterman     |
+| Lena            | Tyndall        |
+| Krissy          | Urrey          |
+| Lizbeth         | Van der Baaren |
+| Lise            | Venner         |
+| Nicolle         | Vosse          |
+| Kathrine        | Wanne          |
+| Annora          | Wegenen        |
+| Gretel          | Wethered       |
+| Chiquia         | Whiten         |
++-----------------+----------------+
+sqlite> 
